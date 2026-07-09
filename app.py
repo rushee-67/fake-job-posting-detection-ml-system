d02f1f237c280753e5ea5944c33dafeab093a52d
Rusheenddra Basani <rusheenddrabasani@gmail.com>
2026-07-10 01:13:14 +0530

    Build Flask web application for model inference

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+
+from fake_job_detector.pipeline.prediction_pipeline import PredictionPipeline
+
+app = Flask(__name__)
+
+pipeline = PredictionPipeline()
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    prediction = None
+    confidence = None
+    if request.method == "POST":
+
+        prediction = pipeline.predict(
+
+            request.form["title"],
+
+            request.form["company_profile"],
+
+            request.form["description"],
+
+            request.form["requirements"],
+
+            request.form["benefits"]
+
+        )
+
+    return render_template(
+        "index.html",
+        prediction=prediction,
+        confidence=confidence
+    )
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
